045da708ae2ff407e221c3cea09074fd51d5fea7
mweiss <michael.josh.weiss@gmail.com>
2016-01-23 19:10:02 +0900

    add all the files I can

--- src/RandomListApp.elm
+++ src/RandomListApp.elm
@@ -1,6 +1,6 @@
-import Html exposing (Html)
+import Html exposing (Html, Attribute)
 import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Html.Events exposing (on)
 import Effects exposing (Effects, Never)
 import Random exposing (int, Seed)
 import StartApp as StartApp
@@ -9,14 +9,20 @@ import Signal.Time as Time2
 import Signal exposing (Signal)
 import Time as Time
 import Array as Array
+import Window as Window
+import Json.Decode as Json
+
 
 -- Time2.startTime, open question: how do I use this without polling for the time,
 -- e.g. just have a signal that goes off once?
 startTimeSeed : Signal Action
 startTimeSeed = Signal.map ((\x -> Init x) << Random.initialSeed << round) (Time.every 100)
 
+windowDimensions : Signal Action
+windowDimensions = Signal.map (\x -> Dimensions x) Window.dimensions
+
 app =
-    StartApp.start { init = init, view = view, update = update, inputs = [startTimeSeed] }
+    StartApp.start { init = init, view = view, update = update, inputs = [startTimeSeed, windowDimensions] }
 
 names = Array.fromList [
   "タフェー",
@@ -51,42 +57,42 @@ port tasks =
 type alias Model = 
   { name : Maybe String
   , seed : Maybe Seed
+  , dimensions : Maybe (Int, Int)
   }
 
-init : (Model, Effects Action)
-init = ({ name = Maybe.Nothing, seed = Maybe.Nothing }, Effects.none)
 
-type Action = DoNothing | Init Seed | Tap
+init : (Model, Effects Action)
+init = ({ name = Maybe.Nothing, dimensions = Maybe.Nothing, seed = Maybe.Nothing }, Effects.none)
+
+type Action = DoNothing | Init Seed | Tap | Dimensions (Int, Int)
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
     DoNothing -> (model, Effects.none)
+    Dimensions x -> ({model | dimensions = Just x }, Effects.none)
     Tap -> case model.seed of
       Nothing -> (model, Effects.none)
       Just s -> let randomSeedPair = (Random.generate (int 0 ((Array.length names) - 1)) s)
-                in ({ model | name = (Array.get (fst randomSeedPair) names)
-                             ,seed = Just (snd randomSeedPair) }
-                    , Effects.none)
+                in ( { model | name = (Array.get (fst randomSeedPair) names)
+                             , seed = Just (snd randomSeedPair) }
+                   , Effects.none)
     Init s -> 
       case model.seed of
         Nothing -> ({ model | seed = Just s }, Effects.none)
         Just _ -> (model, Effects.none)
 
-viewStyles : List (String, String)
-viewStyles = [
-  ("width", "100%")
-  ,("padding-top", "20%")
-  ,("padding-bottom", "20%")
-  ,("text-align", "center")
-  ,("vertical-align", "middle")
-  ,("font-size", "3em")
-  ,("height", "100%")]
-
 view : Signal.Address Action -> Model -> Html
 view address model = 
   let displayText = case model.name of
-    Maybe.Nothing -> "触りなさい"
-    Maybe.Just n -> n
-  in Html.div [onClick address Tap, style viewStyles] [Html.text displayText]
+        Maybe.Nothing -> "タップしてください"
+        Maybe.Just n -> n
+  in Html.div [onTouchDown address, onMouseDown address] [Html.text displayText]
 
+onTouchDown : Signal.Address Action -> Attribute
+onTouchDown address =
+    on "touchstart" Json.value (\_ -> Signal.message address Tap)
+
+onMouseDown : Signal.Address Action -> Attribute
+onMouseDown address =
+    on "mousedown" Json.value (\_ -> Signal.message address Tap)
